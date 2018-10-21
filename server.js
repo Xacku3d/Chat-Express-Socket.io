dfc3e1a919aaafabfc57b2a7fe8b9931f397f03a
Xacku3d <41637751+Xacku3d@users.noreply.github.com>
2018-10-21 20:14:52 +0300

    Version 1.0

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var connections = [];
+var users = [];
 
 const port = process.env.PORT || 3000;
 server.listen(port);
@@ -8,4 +10,35 @@ console.log(`Server is running on port ${port}...`);
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/index.html");
+});
+
+io.sockets.on('connection', function(socket){
+    connections.push(socket);
+    console.log(`Connected: ${connections.length} sockets connected`);
+
+    //Disconnect
+    socket.on('disconnect', function(data) {
+        users.splice(users.indexOf(socket.username), 1);
+        updateUserNames();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log(`Disconnected: ${connections.length} sockets connected`);
+    });
+
+    //Send Message
+    socket.on('send message', function(data){
+        console.log(data);
+        io.sockets.emit('new message', {msg: data, user: socket.username});
+    });
+
+    //New User
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUserNames();
+    });
+
+    function updateUserNames(){
+        io.sockets.emit('get users', users);
+    }
 });
